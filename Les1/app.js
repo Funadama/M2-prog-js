78ad97ef4c15075623076ff12dd4fa4ab619fbf4
Isaac <35624@ma-web.nl>
2022-11-25 16:55:30 +0100

    huisje

--- Les1/app.js
+++ Les1/app.js
@@ -2,15 +2,33 @@ class App
 {
     runApplication()
     {
-        console.log("Hello world!");
-        let appNaam = ("Monkey");
-        let versienummer = (0.2);
-        let versiedatum = ("21/11/2022");
-        let autheur = ("Funadama");
-        let copyright = ("Funadama Studios");
-        let distributeur = ("Funadama");
-        let darkmode = (false);
-        console.log(appNaam, versienummer, versiedatum, autheur, copyright, distributeur, darkmode)
+        console.log("Hello World!");
+        let canvas = document.getElementById("canvasId");
+        let g = canvas.getContext("2d");
+        g.beginPath()
+        g.moveTo(30,10);
+        g.lineTo(70,20);
+        g.lineTo(60,40);
+        g.lineTo(20,30);
+        g.lineTo(30,10);
+        g.moveTo(20,30);
+        g.lineTo(20,50);
+        g.lineTo(60,60);
+        g.lineTo(60,40);
+        g.lineTo(80,30);
+        g.lineTo(70,20);
+        g.moveTo(80,30);
+        g.lineTo(80,50);
+        g.lineTo(60,60);
+        g.moveTo(25,35);
+        g.lineTo(55,42); //
+        g.lineTo(55,55);
+        g.lineTo(25,47);
+        g.lineTo(25,35);
+        g.moveTo(0,0);
+        g.closePath();
+        g.stroke();
+        console.log(canvas)
     }
 }
 
